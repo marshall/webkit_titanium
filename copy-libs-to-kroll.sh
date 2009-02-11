a1f3ac932a9bcb0cb4fc5f5c08f49ffe64da3be1
jservin <servin7@gmail.com>
2009-02-10 22:58:26 -0600

    enable db support in linux

---
+++ copy-libs-to-kroll.sh
@@ -0,0 +1,3 @@
+cp .libs/libwebkit-1.0.so.1.0.0 ../titanium/kroll/thirdparty/linux/webkit/lib
+cp .libs/libwebkit-1.0.lai ../titanium/kroll/thirdparty/linux/webkit/lib
+cp libwebkit-1.0.la ../titanium/kroll/thirdparty/linux/webkit/lib
